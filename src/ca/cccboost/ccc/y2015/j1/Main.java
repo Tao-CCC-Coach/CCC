da59d0dcb14077d448daac34393751847af027a2
Tao Lu <Tao@Taos-MacBook-Air.local>
2017-01-29 12:06:05 -0500

    initial version.

---
+++ src/ca/cccboost/ccc/y2015/j1/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 Tao Lu (Tao.CCC.Coach <at> gmail.com) - All Rights Reserved
+ *
+ * Unauthorized copying of this file is strictly prohibited.
+ *
+ */
+
+package ca.cccboost.ccc.y2015.j1;
+
+/**
+ * Class ca.cccboost.ccc.y2015.j4.Main
+ *
+ * @author <a href="mailto:Tao.CCC.Coach@gmail.com">Tao Lu</a>
+ */
+
+/**
+ * ProblemJ1: SpecialDay
+ ProblemDescription February 18 is a special date for the CCC this year. Writeaprogramthataskstheuserforanumericalmonthandnumericaldayofthemonthandthen determines whether that date occurs before, after, or on February 18. If the date occurs before February 18, output the word Before. If the date occurs after February 18, output the word After. If the date is February 18, output the word Special.
+ InputSpeciﬁcation The input consists of two integers each on a separate line. These integers represent a date in 2015. The ﬁrst line will contain the month, which will be an integer in the range from 1 (indicating January) to 12 (indicating December). The second line will contain the day of the month, which will be an integer in the range from 1 to 31. You can assume that the day of the month will be valid for the given month.
+ OutputSpeciﬁcation Exactly one of Before, After or Special will be printed on one line.
+ SampleInput1 1 7
+ OutputforSampleInput1 Before
+ SampleInput2 8 31
+ OutputforSampleInput2 After
+ SampleInput3 2 18
+ OutputforSampleInput3 Special
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is ca.cccboost.ccc.y2015.j1.Main");
+    }
+}
